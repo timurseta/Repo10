aea214aee80b856628b8b09ff84e1ff8d22b9ad5
timurseta <timur_seta@hotmail.com>
2021-09-22 22:48:09 +0200

    added first object

---
+++ ex01/src/index.js
@@ -0,0 +1,13 @@
+// Only change code below this line
+var myPet = {
+    species: "golden retriver",
+    name: "Luchano",
+    legs: 4,
+    friends: ["Rea", "Coco"]
+}
+function myFunction(myObj) {
+    return myObj;
+}
+// Only change code above this line
+console.log(myFunction(myPet));
+module.exports = { myPet, myFunction };
