3db88afba15978d56ab8fcd8301abc433fa4e7bb
timurseta <timur_seta@hotmail.com>
2021-09-23 09:32:49 +0200

    updated object property

---
+++ ex05/src/index.js
@@ -0,0 +1,17 @@
+// Create school object here
+var school = {
+    name: "Arena",
+    location: "Sarajevo",
+    established: "2020"
+}
+
+// End of school object
+
+function myFunction(name) {
+    // Only change code below this line
+school.name = name;
+return school;
+    // Only change code above this line
+}
+myFunction("Paragon");
+module.exports = { school, myFunction };
